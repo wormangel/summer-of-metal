f795bea499b9a714a961edcdbaa8f3b1d917e2e1
Lucas Azevedo <wormangel@gmail.com>
2017-12-25 20:46:31 +0100

    Add bootstrap, improve UI

--- src/component/FestivalCarrousel/FestivalCarrousel.js
+++ src/component/FestivalCarrousel/FestivalCarrousel.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react';
 import globalFestivalData from '../../util/data.js';
+import Col from 'react-bootstrap/lib/Col';
+import Grid from 'react-bootstrap/lib/Grid';
+import Row from 'react-bootstrap/lib/Row';
 
 class FestivalCarrousel extends Component {
   constructor(props) {
@@ -8,35 +11,64 @@ class FestivalCarrousel extends Component {
       festivals: ["wacken", "hellfest", "metaldays", "partysan", "graspop"],
       currentFestival: 0
     };
+    this.goToNextFestival = this.goToNextFestival.bind(this);
+    this.goToPreviousFestival = this.goToPreviousFestival.bind(this);
   }
 
   render() {
     let festivalData = globalFestivalData[this.state.festivals[this.state.currentFestival]];
     let poster = require(`../../assets/img/posters/${festivalData.poster}`);
     return (
-      <div>
-        <h1>
-          {festivalData.name}
-        </h1>
-        <img src={poster} alt={festivalData.name} />
-      </div>
+      <Grid fluid={true}>
+        <Row>
+          <Col xs={12}>
+            <h1>
+              {festivalData.name}
+            </h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={1}>
+            <div className="left-nav-section">
+              <span className="left-nav-button" onClick={this.goToPreviousFestival} style={{verticalAlign: "center"}}>◀</span>
+            </div>
+          </Col>
+          <Col xs={10}>
+            <div className="carrousel-contents">
+              <img src={poster} alt={festivalData.name}  style={{maxWidth: "100%", height: "auto"}} />
+            </div>
+          </Col>
+          <Col xs={1}>
+            <div className="right-nav-section">
+              <span className="right-nav-button" onClick={this.goToNextFestival}>▶</span>
+            </div>
+          </Col>
+        </Row>
+      </Grid>
     );
   }
 
   setupCarrousel(event) {
-    var nextFestival = 0;
     switch( event.keyCode ) {
       case 37:
-        nextFestival = this.state.currentFestival > 0 ? this.state.currentFestival - 1 : this.state.festivals.length - 1;
-        this.setState({currentFestival: nextFestival});
+        this.goToPreviousFestival();
         break;
       case 39:
-        nextFestival = this.state.currentFestival === this.state.festivals.length - 1 ? 0 : this.state.currentFestival + 1;
-        this.setState({currentFestival: nextFestival});
+        this.goToNextFestival();
         break;
       default:
         break;
     }
+  }
+
+  goToNextFestival() {
+    const nextFestival = this.state.currentFestival === this.state.festivals.length - 1 ? 0 : this.state.currentFestival + 1;
+    this.setState({currentFestival: nextFestival});
+  }
+
+  goToPreviousFestival() {
+    const nextFestival = this.state.currentFestival > 0 ? this.state.currentFestival - 1 : this.state.festivals.length - 1;
+    this.setState({currentFestival: nextFestival});
   }
 
   componentWillMount() {
